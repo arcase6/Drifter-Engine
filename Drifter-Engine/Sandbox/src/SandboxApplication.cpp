77b083f147dff154d746648ddcb162f2d3aaf310
arcase6 <41893575+arcase6@users.noreply.github.com>
2020-11-01 20:56:42 +0900

    Add ExampleLayer to SandboxApplication

--- Drifter-Engine/Sandbox/src/SandboxApplication.cpp
+++ Drifter-Engine/Sandbox/src/SandboxApplication.cpp
@@ -1,8 +1,27 @@
 #include <Drifter.h>
+
+class ExampleLayer : public Drifter::Layer
+{
+public:
+	ExampleLayer() : Layer("Example") {}
+
+	void OnUpdate() override 
+	{
+		DF_LOG_INFO("Example Layer update");
+	}
+
+	void OnEvent(Drifter::Event& e) override
+	{
+		DF_LOG_TRACE("{0}", e);
+	}
+};
+
 class SandboxApplication : public Drifter::Application
 {
 public:
-	SandboxApplication(){}
+	SandboxApplication(){
+		PushLayer(new ExampleLayer());
+	}
 	~SandboxApplication() {}
 };
 
